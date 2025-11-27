7ab9b99dba0e4c4917030a557f69eb49a1a296f2
梁典典 <413153189@qq.com>
2025-11-27 17:40:06 +0800

    initial commit

---
+++ example/android/app/src/main/kotlin/com/example/rusty_dlna_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.rusty_dlna_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ ios/Classes/rusty_dlna.c
@@ -0,0 +1,3 @@
+// Relative import to be able to reuse the C sources.
+// See the comment in ../rusty_dlna.podspec for more information.
+#include "../../src/rusty_dlna.c"
